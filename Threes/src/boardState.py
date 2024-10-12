5c8ff4be3e27082971894cbd823472c8bdc11143
adriantd3 <adriantorremocha@hotmail.com>
2024-10-13 00:15:25 +0200

    Movements fixed
    Defined the methods merge_and replace that takes a row or column and the direction to iterate the array
    
    This method is called for every row or column of the board.
    
    For example, for the moveUp method, we have to check every column from top to down to check every possible merge and place every value to the highest position as possible.

--- Threes/src/boardState.py
+++ Threes/src/boardState.py
@@ -3,14 +3,40 @@ import random
 
 VALUES = [1,1,1,1,1,2,2,3]
 
-
 def check_sum(cell, new_cell):
-    ''' Comprueba si se cumplen las condiciones para hacer la suma'''
+    # Checks whether two cells can be merged
     return (cell + new_cell == 3 or
             (cell == new_cell and
-             cell != 2) or
+             cell >= 3) or
             new_cell == 0)
 
+def merge_and_replace(arr, direction):
+    res = numpy.zeros(4)
+    values = [i for i in arr if i != 0]
+    values_size = len(values)
+
+    # if movement is to the left, it checks from left to right
+    (start, stop, step) = (0,values_size-1,1) if direction == "left" else (values_size-1, 0,-1)
+    
+    if values_size > 1:
+        #Iterate over the array and check if one merge is possible
+        for i in range(start,stop,step):
+            next_index = i+1 if direction == "left" else i-1
+            if check_sum(values[i],values[next_index]):
+                # Merge and remove the next value
+                values[i] = values[i]+ values[next_index]
+                values.pop(next_index)
+                # Only one merge per movement
+                break
+
+    if direction == "left" and len(values) > 0:
+        # Set everything to the left
+        res[:len(values)] = values
+    if direction == "right" and len(values) > 0:
+        # Set everything to the right
+        res[-len(values):] = values
+
+    return res
 
 class BoardState:
 
@@ -30,30 +56,21 @@ class BoardState:
 
 
     def move_up(self):
-        for index, value in numpy.ndenumerate(self.cells):
-            if index[0] > 0 and check_sum(self.cells[index[0], index[1]], self.cells[index[0] - 1, index[1]]):
-                self.cells[index[0] - 1, index[1]] += value
-                self.cells[index[0], index[1]] = 0
+        for col in range(self.n_cols):
+            new_col = merge_and_replace(self.cells[:,col],direction="left")
+            self.cells[:,col] = new_col
 
     def move_down(self):
-        cells_reversed = list(numpy.ndenumerate(self.cells))
-        cells_reversed.reverse()
-        for index, value in cells_reversed:
-            if index[0] < self.n_rows-1 and check_sum(self.cells[index[0], index[1]], self.cells[index[0] + 1, index[1]]):
-                self.cells[index[0] + 1, index[1]] += value
-                self.cells[index[0], index[1]] = 0
+        for col in range(self.n_cols):
+            new_col = merge_and_replace(self.cells[:,col],direction="right")
+            self.cells[:,col] = new_col
 
     def move_left(self):
-        for index, value in numpy.ndenumerate(self.cells):
-            if index[1] > 0 and check_sum(self.cells[index[0], index[1]], self.cells[index[0], index[1] - 1]):
-                self.cells[index[0], index[1] - 1] += value
-                self.cells[index[0], index[1]] = 0
+        for row in range(self.n_rows):
+            new_row = merge_and_replace(self.cells[row,:],direction="left")
+            self.cells[row,:] = new_row
 
     def move_right(self):
-        cells_reversed = list(numpy.ndenumerate(self.cells))
-        cells_reversed.reverse()
-        for index, value in cells_reversed:
-            if index[1] < self.n_cols-1 and check_sum(self.cells[index[0], index[1]], self.cells[index[0], index[1] + 1]):
-                self.cells[index[0], index[1] + 1] += value
-                self.cells[index[0], index[1]] = 0
-
+        for row in range(self.n_rows):
+            new_row = merge_and_replace(self.cells[row,:],direction="right")
+            self.cells[row,:] = new_row
